9b4561b2f3076f4bac009d19becadbfa057edec5
ratneshankam <ankamratnesh@gmail.com>
2025-01-05 12:51:47 +0530

    adding outputs

---
+++ RTR/Practice/example.c
@@ -0,0 +1,49 @@
+// // #include <stdio.h>
+
+// // int x;
+
+// int main(void)
+// {
+//     // printf("Hello, World!");
+
+//     // int a;
+//     // char c = '\0';
+
+//     // const int const_value = 34;
+
+//     // printf("x = %d", x);
+//     // printf("a = %d", a);
+
+//     int a = 5;
+//     int b = 12;
+
+//     // float ans = a + b - a * a / b;
+
+//     // int ans = a > b - a;
+
+//     /*
+//     logical AND
+//     1 0 -> 0
+//     0 1 -> 0
+//     0 0 -> 0
+//     1 1 -> 1
+//     */
+//     /*
+//     logical OR
+//     1 0 -> 1
+//     0 1 -> 1
+//     0 0 -> 0
+//     1 1 -> 1
+//     */
+
+//     b = 0;
+
+//     int ans = !b;
+
+//     printf("answer : %d", ans);
+
+//     return (0);
+// }
+
+
+
